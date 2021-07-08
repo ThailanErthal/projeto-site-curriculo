b67776a17e3e4dd10b65bb230cbe08c5dc925062
ThailanErthal <taerthal2@hotmail.com>
2021-07-08 19:28:54 -0300

    atualizacoes-08-07

--- ci/site/application/controllers/projeto.php
+++ ci/site/application/controllers/projeto.php
@@ -1,13 +1,25 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); //Sempre deixar definido, pois se não achar o diretório vai aparecer a mensagem.
 
-class Projeto extends CI_Controller { //A classe sempre deve ter o mesmo nome do arquivo controller
+class Projeto extends MY_Controller { //A classe sempre deve ter o mesmo nome do arquivo controller
 
-	function __contrruct(){ //construdor do controller, nele é possivel carregar as librari, helpers, models que serão utilizados nesse controller
-		parent::__contrruct();//Chamando o construtor da classe pai
+	function __construct() {
+		parent::__construct();
 	}
 
 	public function index(){
-		$this->load->helper('url');
-		$this->load->view('index');
+		$this->render('index');
+		$this->load->library('email');
+		$dados_form = $this->input->post();
+		$this->email->from($dados_form['email'], $dados_form['nome']);
+		$this->email->to('multittam@gmail.com');
+		$this->email->subject($dados_form['assunto']);
+		$this->email->message($dados_form['mensagem']);
+        $this->email->send();
+		
+		
+		print_r($dados_form);
+		
+		
 	}
 }
+
